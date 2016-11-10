a53212f782b0fc098d265c2997a84ccb14afd14d
arvin <arvin@turingcat.com>
2016-11-10 14:10:09 +0800

    基础框架以后可以直接使用 第一次使用maven 配置 web

---
+++ src/main/java/com/waddwaw/demo/Hello.java
@@ -0,0 +1,18 @@
+package com.waddwaw.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by arvin on 2016/11/10.
+ */
+@Controller
+
+public class Hello {
+    @RequestMapping("/hello")
+    public String printWelcome(ModelMap model) {
+        model.addAttribute("message", "Hello world!");
+        return "/hello.ftl";
+    }
+}
